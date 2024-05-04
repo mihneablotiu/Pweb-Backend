0a95e86434cd9d76903d2b41fcd1884da1ab2cff
Mihnea-Andrei BLOȚIU <mihnea.blotiu@stud.acs.upb.ro>
2024-05-04 12:32:57 +0300

    Backend done

--- src/main/java/com/pweb/springBackend/configs/CorsConfig.java
+++ src/main/java/com/pweb/springBackend/configs/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Add your frontend domain here
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175",
+                        "http://localhost:5176", "http://localhost:5177", "http://localhost:5178",
+                        "http://localhost:5179", "http://localhost:5180") // Add your frontend domain here
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
